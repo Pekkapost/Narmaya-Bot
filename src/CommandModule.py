9bdd11e4e237821f75ef50d539e3f4b0f0576d55
ericshen9999 <dragoneshen@gmail.com>
2024-03-15 13:31:30 -0700

    Add

--- src/CommandModule.py
+++ src/CommandModule.py
@@ -78,6 +78,16 @@ class FarmData(commands.Cog):
         item = "curios"
         await ctx.send(embed=self.getEmbed(item))
 
+    @commands.command(
+        aliases=["weaponmaterials","weaponmats","wepmats"],
+        help=database["faqdata"]["weaponmaterials"]["help"],
+        brief=database["faqdata"]["weaponmaterials"]["help"]
+    )
+    async def weapon_materials(self, ctx):
+        # Item to find
+        item = "weaponmaterials"
+        await ctx.send(embed=self.getEmbed(item))
+
 class FAQ(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -176,16 +186,6 @@ class FAQ(commands.Cog):
     async def pins(self, ctx):
         # Item to find
         await ctx.send("Read the Pins")
-    
-    @commands.command(
-            aliases=["weaponmaterials","weaponmats","wepmats"],
-            help=database["faqdata"]["weaponmaterials"]["help"],
-            brief=database["faqdata"]["weaponmaterials"]["help"]
-    )
-    async def weapon_materials(self, ctx):
-        # Item to find
-        item = "weaponmaterials"
-        await ctx.send(embed=self.getEmbed(item))
 
 class Characters(commands.Cog):
     def __init__(self, client):
